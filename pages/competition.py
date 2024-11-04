7182ab12a5e1d585cdc9b15d371cf324913f8727
Bunrawat Charoenyuennan <158060569+Pomaccel@users.noreply.github.com>
2024-11-04 13:45:08 +0000

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:    .streamlit/config.toml #	modified:   pages/competition.py #	modified:   streamlit_app.py

--- pages/competition.py
+++ pages/competition.py
@@ -1,16 +1,14 @@
 import streamlit as st
 
-
 def main():
     # builds the sidebar menu
     with st.sidebar:
-        st.page_link('streamlit_app.py', label='Individual Checker', icon='🔥')
-        st.page_link('pages/competition.py', label='Competition Checker', icon='🛡️')
+        st.page_link('streamlit_app.py', label='Home', icon='🔥')
+        st.page_link('pages/competition.py', label='Member', icon='🛡️')
 
-    st.title(f'🛡️ Competition Checker')
+    st.title(f'🛡️ Member')
 
     # your content
-
 
 if __name__ == '__main__':
     main()
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import streamlit as st
 def main():
     # builds the sidebar menu
     with st.sidebar:
-        st.page_link('streamlit_app.py', label='Individual Checker', icon='🔥')
-        st.page_link('pages/competition.py', label='Competition Checker', icon='🛡️')
+        st.page_link('streamlit_app.py', label='Home', icon='🔥')
+        st.page_link('pages/competition.py', label='Member', icon='🛡️')
 
-    st.title(f'🔥 Individual Checker')
+    st.title(f'🔥 Home')
 
     # your content
 if __name__ == '__main__':
