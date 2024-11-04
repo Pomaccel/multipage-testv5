af569484636b84cddd2084f7fd2f6c4f03be3fd8
Bunrawat Charoenyuennan <158060569+Pomaccel@users.noreply.github.com>
2024-11-04 13:26:30 +0000

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	new file:    .streamlit/config.toml #	new file:   pages/competition.py #	modified:   streamlit_app.py

--- pages/competition.py
+++ pages/competition.py
@@ -7,8 +7,10 @@ def main():
         st.page_link('streamlit_app.py', label='Individual Checker', icon='🔥')
         st.page_link('pages/competition.py', label='Competition Checker', icon='🛡️')
 
-    st.title(f'🔥 Individual Checker')
+    st.title(f'🛡️ Competition Checker')
 
     # your content
+
+
 if __name__ == '__main__':
     main()
